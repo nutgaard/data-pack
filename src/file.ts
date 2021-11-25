a261a8e75b9fbedd500dd98c234992b72b7671c4
Nicklas Utgaard <nutgaard@gmail.com>
2021-11-25 01:59:12 +0100

    feat: add file utilities

---
+++ src/file.ts
@@ -0,0 +1,30 @@
+import * as fs from 'fs';
+import { WriteStream } from 'fs';
+import * as fsP from 'fs/promises';
+import { serialize, deserialize } from './json';
+
+export async function read(path: string): Promise<string> {
+    return fsP.readFile(path, 'utf8');
+}
+export function readSync(path: string): string {
+    return fs.readFileSync(path, 'utf8');
+}
+export async function readJSON(path: string): Promise<string> {
+    return fsP.readFile(path, 'utf8').then(deserialize);
+}
+export function readJSONSync(path: string): string {
+    return deserialize(fs.readFileSync(path, 'utf8'));
+}
+
+export async function write(path: string, content: any): Promise<void> {
+    return fsP.writeFile(path, serialize(content), 'utf8');
+}
+export function writeSync(path: string, content: any): void {
+    return fs.writeFileSync(path, serialize(content), 'utf8');
+}
+
+export function withWriteStream(path: string, block: (stream: WriteStream) => void): void {
+    const writeStream = fs.createWriteStream(path, 'utf8');
+    block(writeStream);
+    writeStream.close();
+}
