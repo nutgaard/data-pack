e69bc653785b52184af2fca11177083360de3b56
Nicklas Utgaard <nutgaard@gmail.com>
2023-01-18 15:01:35 +0100

    fix(expose): expose kv serializer to public

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as File from './file';
 export * as CSV from './dataformat/csv';
 export * as Json from './dataformat/json';
+export * as Kv from './dataformat/kv';
 export { GeneratorFlow } from './generator-flow';
